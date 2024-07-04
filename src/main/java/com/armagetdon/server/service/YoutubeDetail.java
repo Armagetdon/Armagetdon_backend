c1672840d6e1811eb2ced499badcd3b8cf8124f4
= <=>
2024-07-04 18:00:30 +0900

    feat: YoutubeDetail Response 구현

---
+++ src/main/java/com/armagetdon/server/service/YoutubeDetail.java
@@ -0,0 +1,11 @@
+package com.armagetdon.server.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class YoutubeDetail {
+    private String title;
+    private String thumbnailUrl;
+}
