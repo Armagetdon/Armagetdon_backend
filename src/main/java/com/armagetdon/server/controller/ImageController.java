3948c2f6f371b147c015f6457c0dd96b0257ff61
yumzen <yeom.ji.eun35@gmail.com>
2024-07-05 02:52:45 +0900

    feat: swagger 관리

--- src/main/java/com/armagetdon/server/controller/ImageController.java
+++ src/main/java/com/armagetdon/server/controller/ImageController.java
@@ -2,6 +2,8 @@ package com.armagetdon.server.controller;
 
 import com.armagetdon.server.apiPayload.ApiResponse;
 import com.armagetdon.server.service.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,9 +13,11 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/images")
+@Tag(name="Image", description = "이미지 업로드 및 삭제 기능 API")
 public class ImageController {
     private final ImageService imageService;
 
+    @Operation(summary = "이미지 업로드 기능 API", description = "게시글 생성 - 파일 선택 시 실행하는 API")
     @PostMapping("")
     public ApiResponse<String> uploadImage(@RequestPart(value = "file", required = false) MultipartFile multipartFile){
         try {
@@ -23,6 +27,7 @@ public class ImageController {
         }
     }
 
+    @Operation(summary = "이미지 삭제 기능 API", description = "파일 선택 후 뒤로가기 버튼 터치 시 실행하는 API")
     @DeleteMapping("/{id}")
     public ApiResponse<?> deleteImage(@PathVariable("id") Long image_id) {
         try {
