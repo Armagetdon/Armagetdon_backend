7311f6acd8170e49d3c6d770ab4aafe0f9eef76c
ssongmina <thdalsdk5498@naver.com>
2024-07-05 01:22:10 +0900

    [feat] 게시글 생성 API 추가

--- src/main/java/com/armagetdon/server/converter/PostConverter.java
+++ src/main/java/com/armagetdon/server/converter/PostConverter.java
@@ -1,18 +1,29 @@
 package com.armagetdon.server.converter;
 
+import com.armagetdon.server.apiPayload.code.status.ErrorStatus;
+import com.armagetdon.server.apiPayload.exception.handler.PostHandler;
 import com.armagetdon.server.domain.Post;
 import com.armagetdon.server.domain.PostImage;
 import com.armagetdon.server.dto.PostRequestDTO;
 import com.armagetdon.server.dto.PostResponseDTO;
+import com.armagetdon.server.repository.ImageRepository;
 import com.armagetdon.server.service.PostCommandServiceImpl;
+
+import java.util.Optional;
 
 
 public class PostConverter {
 
     private static PostCommandServiceImpl postCommandService;
+    private static ImageRepository ImageRepository;
     // 게시글 생성 API
     public static Post toPost(PostRequestDTO.createPostDTO request){
-        PostImage image = postCommandService.giveImage(request.getPost_image_id());
+        //PostImage image = postCommandService.giveImage(request.getPost_image_id();
+
+        System.out.println(request.getPost_image_id());
+
+        Optional<PostImage> optionalimage = ImageRepository.findById(request.getPost_image_id());
+        PostImage image = optionalimage.orElseThrow(()-> new PostHandler(ErrorStatus.IMAGE_NOT_FOUND));
         return Post.builder()
                 .post_image_id(image)
                 .youtube_url(request.getYoutube_url())
--- src/main/java/com/armagetdon/server/dto/PostRequestDTO.java
+++ src/main/java/com/armagetdon/server/dto/PostRequestDTO.java
@@ -1,6 +1,7 @@
 package com.armagetdon.server.dto;
 
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -14,7 +15,7 @@ public class PostRequestDTO {
         @NotBlank
         private String youtube_url;
 
-        @NotNull
+        @Min(1)
         private Long post_image_id;
     }
 }
