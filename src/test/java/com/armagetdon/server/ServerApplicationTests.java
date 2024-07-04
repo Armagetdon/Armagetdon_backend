624fe9396e83514e279ba91bac187a9b1c9594d5
= <=>
2024-07-04 16:33:29 +0900

    initial project

---
+++ src/test/java/com/armagetdon/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armagetdon.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
